c60ef1f8a16912d7fae27f33c93bd07c9d5e7fc0
yaojie <yaojie@ihealthlabs.com.cn>
2019-11-12 09:34:26 +0800

    双指针

---
+++ leetcode524.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s = "abpcplea"
+	var d = []string{"a", "b", "c"}
+	fmt.Println("----------", findLongestWord(s, d))
+}
+
+func findLongestWord(s string, d []string) string {
+	longest := ""
+	for _, v := range d {
+		if len(v) > len(longest) || (len(v) == len(longest) && v < longest) {
+			if isSubStr(s, v) {
+				longest = v
+			}
+		}
+	}
+	return longest
+}
+
+func isSubStr(s string, t string) bool {
+	index1, index2 := 0, 0
+	for index1 < len(s) && index2 < len(t) {
+		if s[index1] == t[index2] {
+			index2++
+		}
+		index1++
+	}
+	if index2 == len(t) {
+		return true
+	}
+	return false
+}
