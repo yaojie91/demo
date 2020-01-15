2d7b5fb30ee5e4cbfe01f3a21723e646620c93c9
yaojie <yaojie425@163.com>
2020-01-15 15:26:36 +0800

    读文件行数

--- io/fileLine/fileLine.go
+++ io/fileLine/fileLine.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func main() {
+	if len(os.Args) < 2 {
+		return
+	}
+
+	fileName := os.Args[1]
+	file, err := os.Open(fileName)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	r := bufio.NewReader(file)
+
+	var line int
+
+	for {
+		_, isPrefix, err := r.ReadLine()
+		if err != nil {
+			break
+		}
+		if !isPrefix {
+			line++
+		}
+	}
+
+	fmt.Println(line)
 }
