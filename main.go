5d92b849da7692e4458d21fff965f4698aee09c3
姚杰 <yaojie425@163.com>
2020-03-31 22:26:27 +0800

    回滚

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
+func ReadFrom(reader io.Reader, num int) ([]byte, error) {
+	p := make([]byte, num)
+	n, err := reader.Read(p)
+	if n > 0 {
+		return p[:n], nil
+	}
+	return p, err
+}
+
+func SampleReadStdin() {
+	fmt.Println("please enter:")
+	data, err := ReadFrom(os.Stdin, 10)
+	if err != nil {
+		return
+	}
+	fmt.Println("---------", data)
+}
+
+func main() {
+	SampleReadStdin()
 }
