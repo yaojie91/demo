135b020a4f8da380f1e3979b112647cdd5179629
姚杰 <yaojie425@163.com>
2020-04-28 07:46:21 +0800

    抢占测试

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+func process(id int) {
+	fmt.Println(id)
+	for {
+	}
+}
+func main() {
+	var wg sync.WaitGroup
+	n := 10
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go process(i)
+	}
+	wg.Wait()
 }
